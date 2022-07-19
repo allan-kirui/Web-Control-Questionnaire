98cb8543d9bb64b3dd26b21e596c0237ea2e97e7
allan-kirui <allan.kipkemoi.kirui@gmail.com>
2022-07-19 08:45:13 +0200

    Various functionality modifications and Bug fixes
    Added various constants
    
    Added locateAndConfirmSend fucntion, read comments
    
    Modified the search text for locating questionnaires,
    that should be skipped i.e MODUŁU 2020
    
    Sped up the ticking of checkboxes wait time
    
    Fixed bug of differentiating between send button,
    and confirm send button
    
    [Ticket: #4]
    
    Took 2 hours 4 minutes

--- main.py
+++ main.py
@@ -5,8 +5,24 @@ from selenium.webdriver.edge.service import Service
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
 from selenium.webdriver.common.by import By
 from passManagement import pwd, username  # used passlib to hash password
-# from selenium.webdriver.support import expected_conditions as EC
-# from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+NORMAL_WAIT_TIME = 5  # Seconds to wait
+FAST_WAIT_TIME = 2
+AJAX_WAIT_TIME = 20
+NUM_OF_CHECKBOXES = 9
+
+
+def locateAndConfirmSend(driver):
+    # Locating and clicking the confirm button
+    wyslijConfirm = driver.find_element(By.XPATH, '//*[contains(@id,"i1:j_id")][contains(@value,"odpowiedź")]')
+    print(wyslijConfirm.text, wyslijConfirm.accessible_name, wyslijConfirm.location)
+    locationWyslijConfirm = wyslijConfirm.location_once_scrolled_into_view
+    driver.execute_script("arguments[0].click();", WebDriverWait(driver, AJAX_WAIT_TIME).until(
+        EC.element_to_be_clickable((By.XPATH, '//*[contains(@id,"i1:j_id")][contains(@value,"odpowiedź")]'))))
+    time.sleep(FAST_WAIT_TIME)
+
 
 driver = webdriver.Edge(service=Service(EdgeChromiumDriverManager().install()))
 
@@ -46,12 +62,11 @@ try:
     # Click proceed_button
     proceed_button.click()
 
-    time.sleep(0)
     student_button = driver.find_element(By.XPATH, '//a[@href="' + '/auth/app/student' + '"]')
 
     student_button.click()
     questionnaire_button = None
-    time.sleep(5)
+    time.sleep(NORMAL_WAIT_TIME)
     toggle = driver.find_element(By.XPATH, '//a[@rel="' + 'menu-start' + '"]')
 
     try:
@@ -65,71 +80,58 @@ try:
         questionnaire_button = driver.find_element(By.ID, "j_id191")
         questionnaire_button.click()
 
-    # Locating all 2020 modules to click link
-    time.sleep(5)
+    time.sleep(NORMAL_WAIT_TIME)
 
+    # Calculates the number of questionnaires
     allEven = driver.find_elements(By.XPATH, '//*[contains(@class,"dtEvenRow")]')
     allEven = len(allEven)
     allOdd = driver.find_elements(By.XPATH, '//*[contains(@class,"dtOddRow")]')
     allOdd = len(allOdd)
     numOfRows = allOdd + allEven
 
+    # Goes through each row, and performs appropriate actions for each row
     for row in range(numOfRows):
+        # Selects first row
         firstRow = driver.find_element(By.XPATH, '//*[@id="i1:edt_polls_tbl:tb"]/tr[1]')
+        locationFirstRow = firstRow.location_once_scrolled_into_view
         firstRow.click()
-        time.sleep(5)
+        time.sleep(NORMAL_WAIT_TIME)
+
+        # Gets the title of the questionnaire
         title = driver.find_element(By.XPATH, '//*[@id="i1"]/div[1]/div/h1/span')
         print(title.text)
 
-        if str(2020) in title.text:
+        if "MODUŁU 2020" in title.text:  # Indicates that this is an old questionnaire a.k.a not meant to be filled by us
             ankietaNieDotyczy = driver.find_element(By.XPATH, '//*[contains(@value,"dotyczy")]')
-            print(ankietaNieDotyczy.text)
-            location = ankietaNieDotyczy.location_once_scrolled_into_view
+            print(ankietaNieDotyczy.text, ankietaNieDotyczy.accessible_name, ankietaNieDotyczy.location)
+            location = ankietaNieDotyczy.location_once_scrolled_into_view  # Scrolls to the element
             ankietaNieDotyczy.click()
+
+            # Locates the confirm send button
+            locateAndConfirmSend(driver)
         else:
-            # ticking the checkboxes
-            for index in range(9):
-                # checkboxes = driver.find_element(By.XPATH, '//*[contains(@id,"'+str(0)+':pnl_qClosedSingle")]')
-                # print(checkboxes.text)
-                checkboxA = driver.find_element(By.XPATH, '//*[contains(@id,"questions_panel:' + str(index) + '")]//*[contains(@id,"'+str(0)+':pnl_qClosedSingle")]')
+            # Ticking the checkboxes
+            for index in range(NUM_OF_CHECKBOXES):
+                # Locates the checkbox
+                checkboxA = driver.find_element(By.XPATH, '//*[contains(@id,"questions_panel:' + str(
+                    index) + '")]//*[contains(@id,"' + str(0) + ':pnl_qClosedSingle")]')
                 print(checkboxA.text)
-                location = checkboxA.location_once_scrolled_into_view
+                locationCheckboxA = checkboxA.location_once_scrolled_into_view
                 checkboxA.click()
-                time.sleep(5)
+                time.sleep(FAST_WAIT_TIME)
 
+            # Sends our filled questionnaire
             wyslijButton = driver.find_element(By.XPATH, '//*[contains(@value,"odpowiedź")]')
-            print(wyslijButton.text)
-            location = wyslijButton.location_once_scrolled_into_view
+            print(wyslijButton.text, wyslijButton.location)
+            locationWyslijButton = wyslijButton.location_once_scrolled_into_view
             wyslijButton.click()
-            time.sleep(3)
-            wyslijConfirm = driver.find_element(By.XPATH,'//*[contains(@id,"i1:j_id")]')
-            print(wyslijConfirm.text)
-            wyslijConfirm.click()
+            time.sleep(NORMAL_WAIT_TIME)
 
-    # modules = driver.find_elements(By.XPATH, '//span[contains(text(),'+'OCENY'+')]')
-    # for module in modules:
-    #     print(module.text)
-    #     parent_of_module = module.find_element(By.XPATH, '..')
-    #     print(parent_of_module.text)
-    #     not_involving = parent_of_module.find_element(By.XPATH,  '//span[contains(text(),'+str(2020)+')]' and '//a[@class="' + 'stopEventPropagation' + '"]' )
-    #     print(not_involving.text)
-    #     not_involving.click()
-    #     time.sleep(5)
-    #
-    #     not_involving_check = parent_of_module.find_element(By.XPATH, '//span[contains(text(),' + str(2020) + ')]').text
-    #     print(not_involving_check)
-    #     if str(2020) in not_involving_check:
-    #         wyslij_button = driver.find_element(By.CLASS_NAME, 'simpleButton')
-    #         wyslij_button.click()
-    #     else:
-    #         anuluj_button = driver.find_element(By.CLASS_NAME, 'leftSpace')
-    #         anuluj_button.click()
-    #
-
-    # TODO: go through whole list if it contains 2020 accept deletions.
-    # TODO: if not, ignore, then another loop to fill in questionnaire
+            # Locating and clicking the confirm button
+            locateAndConfirmSend(driver)
 
     driver.quit()
 
-except:
+except Exception as e:
+    print(e)
     driver.quit()
