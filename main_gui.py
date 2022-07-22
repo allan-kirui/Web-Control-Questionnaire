30e32063247e3a6f1475a3719a60a768e3838526
allan-kirui <allan.kipkemoi.kirui@gmail.com>
2022-07-22 16:33:21 +0200

    Added GUI functionality
    Clicking of buttons and input in textfield is now,
    connected to the backend
    
    [Ticket: #13]
    
    Took 23 minutes

--- main_gui.py
+++ main_gui.py
@@ -1,14 +1,33 @@
 import sys
 from PyQt5.uic import loadUi
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QDialog, QApplication, QWidget, QStackedWidget
+from PyQt5.QtWidgets import QDialog, QApplication, QWidget, QStackedWidget, QFileDialog
+
+from main import Questionnaire
 
 
 class WelcomeScreen(QDialog):
     def __init__(self):
         super(WelcomeScreen, self).__init__()
         loadUi("main_dialog.ui",self)
+        self.passwordLineEdit.setEchoMode(QtWidgets.QLineEdit.Password)
 
+        self.browse.clicked.connect(self.browsefiles)
+        self.submitButton.clicked.connect(self.submit)
+
+    def browsefiles(self):
+        fname = QFileDialog.getOpenFileName(self,'Pick file with list of Professors')
+        self.filename.setText(fname[0])
+
+    def submit(self):
+        username = self.usernameLineEdit.text()
+        password = self.passwordLineEdit.text()
+        filepath = self.filename.text()
+
+        if len(username) != 7 or len(password) == 0 or len(filepath) == 0:
+            self.error.setText("Please input all fields")
+        else:
+            Questionnaire(username,password,filepath)
 
 #menu
 app = QApplication(sys.argv)
@@ -16,7 +35,7 @@ welcome = WelcomeScreen()
 widget = QStackedWidget()
 widget.addWidget(welcome)
 widget.setFixedHeight(600)
-widget.setFixedWidth(800)
+widget.setFixedWidth(900)
 widget.show()
 try:
     sys.exit(app.exec())
